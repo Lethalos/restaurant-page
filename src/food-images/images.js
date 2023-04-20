cc4758b9360fb2e5084f762fa0eaf01ce603cc8c
Lethalos <necdetkaanoz@hotmail.com>
2023-04-20 18:03:06 +0300

    Add CSS and JS codes

---
+++ src/food-images/images.js
@@ -0,0 +1,12 @@
+const Images = {
+  food1: require("./food-1.jpeg"),
+  food2: require("./food-2.jpeg"),
+  food3: require("./food-3.jpeg"),
+  food4: require("./food-4.jpeg"),
+  food5: require("./food-5.jpeg"),
+  food6: require("./food-6.jpeg"),
+  food7: require("./food-7.jpeg"),
+  food8: require("./food-8.jpeg"),
+};
+
+export default Images;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import _, { add, forEach } from "lodash";
+import "./style.css";
+
+import FoodImages from "./food-images/images.js";
+
 const header = document.createElement("header");
 header.classList.add("navbar");
 
@@ -11,6 +16,12 @@ document.body.appendChild(content);
 document.body.appendChild(footer);
 
 // Header Buttons
+const title = document.createElement("h1");
+title.innerText = "Keep Fit";
+
+const buttons = document.createElement("div");
+buttons.classList.add("buttons");
+
 const homeBtn = document.createElement("button");
 homeBtn.innerText = "Home";
 homeBtn.id = "home";
@@ -23,55 +34,108 @@ const contactBtn = document.createElement("button");
 contactBtn.innerText = "Contact";
 contactBtn.id = "contact";
 
-header.appendChild(homeBtn);
-header.appendChild(menuBtn);
-header.appendChild(contactBtn);
+header.appendChild(title);
+header.appendChild(buttons);
+buttons.appendChild(homeBtn);
+buttons.appendChild(menuBtn);
+buttons.appendChild(contactBtn);
 
 // Home Page
 const homePage = document.createElement("div");
 homePage.id = "homePage";
 
 const homeTitle = document.createElement("h1");
-homeTitle.innerText = "Welcome to the Home Page";
+homeTitle.innerText = "EASY TO BE HEALTHY";
 
 const homeText = document.createElement("p");
 homeText.innerText =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae aliquam lacinia, nisl nisl aliquet nisl, eget aliquam nis";
+  "Healthy doesn’t have to be hard. We’re the health nuts, so you don’t have to be";
+
+const homeMenuBtn = document.createElement("button");
+homeMenuBtn.classList.add("homeMenuBtn");
+homeMenuBtn.innerText = "Checkout the Menu";
 
 homePage.appendChild(homeTitle);
 homePage.appendChild(homeText);
+homePage.appendChild(homeMenuBtn);
 
 // Menu Page
 const menuPage = document.createElement("div");
 menuPage.id = "menuPage";
 
 const menuTitle = document.createElement("h1");
-menuTitle.innerText = "Welcome to the Menu Page";
+menuTitle.innerText = "Menu";
 
-const menuText = document.createElement("p");
-menuText.innerText =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae aliquam lacinia, nisl nisl aliquet nisl, eget aliquam nis";
+const foods = document.createElement("div");
+foods.classList.add("foods");
+
+forEach(FoodImages, (value, key) => {
+  const foodCard = document.createElement("div");
+  foodCard.classList.add("foodCard");
+
+  const food = new Image();
+  food.src = value;
+
+  const cardTitle = document.createElement("h2");
+  cardTitle.innerText = key;
+
+  const cardDescription = document.createElement("p");
+  cardDescription.innerText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae aliquam lacinia, nisl nisl aliquet nisl, eget aliquam nis";
+
+  foodCard.appendChild(food);
+  foodCard.appendChild(cardTitle);
+  foodCard.appendChild(cardDescription);
+
+  foods.appendChild(foodCard);
+});
 
 menuPage.appendChild(menuTitle);
-menuPage.appendChild(menuText);
+menuPage.appendChild(foods);
 
 // Contact Page
 const contactPage = document.createElement("div");
 contactPage.id = "contactPage";
 
 const contactTitle = document.createElement("h1");
-contactTitle.innerText = "Welcome to the Contact Page";
+contactTitle.classList.add("contactTitle");
+contactTitle.innerText = "Contact Us";
 
-const contactText = document.createElement("p");
-contactText.innerText =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae aliquam lacinia, nisl nisl aliquet nisl, eget aliquam nis";
+const phone = document.createElement("p");
+phone.classList.add("phone");
+phone.innerText = "Phone: 123-456-7890";
+
+const email = document.createElement("p");
+email.classList.add("email");
+email.innerText = "Email: keepfit@gmail.com";
+
+const address = document.createElement("p");
+address.classList.add("address");
+address.innerText = "Address: 1234 Main St, City, State, 12345";
+
+const map = document.createElement("iframe");
+map.classList.add("map");
+map.src =
+  "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d84726.42903603791!2d-81.39880118385516!3d29.58825511842685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1str!2str!4v1682000240104!5m2!1str!2str";
 
 contactPage.appendChild(contactTitle);
-contactPage.appendChild(contactText);
+contactPage.appendChild(phone);
+contactPage.appendChild(email);
+contactPage.appendChild(address);
+contactPage.appendChild(map);
 
 // Footer
 const footerText = document.createElement("p");
-footerText.innerText = "Created by: Kaan Ozdogan";
+footerText.classList.add("footerText");
+footerText.innerText = "Created by: ";
+
+const footerLink = document.createElement("a");
+footerLink.classList.add("footerLink");
+footerLink.innerText = "Kaan Ozdogan";
+footerLink.href = "https://github.com/Lethalos";
+footerLink.target = "_blank";
+
+footerText.appendChild(footerLink);
 
 footer.appendChild(footerText);
 
@@ -82,15 +146,19 @@ homeBtn.addEventListener("click", () => {
 });
 
 menuBtn.addEventListener("click", () => {
-    content.innerHTML = "";
-    content.appendChild(menuPage);
+  content.innerHTML = "";
+  content.appendChild(menuPage);
 });
 
 contactBtn.addEventListener("click", () => {
-    content.innerHTML = "";
-    content.appendChild(contactPage);
+  content.innerHTML = "";
+  content.appendChild(contactPage);
+});
+
+homeMenuBtn.addEventListener("click", () => {
+  content.innerHTML = "";
+  content.appendChild(menuPage);
 });
 
 // Default Page
 content.appendChild(homePage);
-
